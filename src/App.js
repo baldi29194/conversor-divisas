a3b9d0af4510781157420df290344fd3ced50213
baldi29194 <baldi29194@gmail.com>
2023-09-05 00:13:23 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <h1>Conversor divisas con <a href="https://www.frankfurter.app/">FrankFurter API</a>.</h1>
       <Calcular />
     </div>
-    <h2 className='creador'>Creado y codificado por <a href="https://oscaromargn.github.io/portafolio">Oscar Garcia</a>.</h2>
+   
     </>
   );
 }
